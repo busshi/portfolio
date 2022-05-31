54211e8551c0e24b3e1c129112b775d30da63ad4
busshi <busshi973@icloud.com>
2022-05-31 15:33:49 +0200

    testing jsonld

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,83 +1,84 @@
-export const EMAIL = 'contact@busshi.fr'
-export const SITE_PRO = 'https://busshidev.fr'
-export const AVATAR_URL = '/assets/authors/busshi.jpeg'
+export const EMAIL = "contact@busshi.fr";
+export const SITE = "https://busshi.fr";
+export const SITE_PRO = "https://busshidev.fr";
+export const AVATAR_URL = "/assets/authors/busshi.jpeg";
 export const SITE_TITLE = [
-    {
-        lang: 'EN',
-        content: 'My portfolio'
-    },
-    {   lang: 'FR',
-        content: 'Mon portfolio'
-    }
-]
+  {
+    lang: "EN",
+    content: "My portfolio",
+  },
+  { lang: "FR", content: "Mon portfolio" },
+];
 export const STORIES_TITLE = [
-    {
-        lang: 'EN',
-        content: 'Some projects I made'
-    },
-    {   lang: 'FR',
-        content: 'Mes dernières réalisations'
-    }
-]
+  {
+    lang: "EN",
+    content: "Some projects I made",
+  },
+  { lang: "FR", content: "Mes dernières réalisations" },
+];
 export const FOOTER = [
-    {
-        lang: 'EN',
-        slogan: 'Powered by busshi',
-        contact: 'Contact me',
-        company: 'Check my company here'
-    },
-    {   lang: 'FR',
-        slogan: 'Développé par busshi',
-        contact: 'Me contacter',
-        company: 'Site pro'
-    }
-]
+  {
+    lang: "EN",
+    slogan: "Powered by busshi",
+    contact: "Contact me",
+    company: "Check my company here",
+  },
+  {
+    lang: "FR",
+    slogan: "Développé par busshi",
+    contact: "Me contacter",
+    company: "Site pro",
+  },
+];
 export const POST_FOOTER = [
-    {
-        lang: 'EN',
-        frameworks: 'Frameworks',
-        languages: 'Languages',
-        dbs: 'Databases',
-        devTools: 'Development tools',
-        deployTools: 'Deployment tools',
-    },
-    {   lang: 'FR',
-        frameworks: 'Frameworks',
-        languages: 'Langages',
-        dbs: 'Bases de données',
-        devTools: 'Outils de développement',
-        deployTools: 'Outils de déploiement',
-}
-]
+  {
+    lang: "EN",
+    frameworks: "Frameworks",
+    languages: "Languages",
+    dbs: "Databases",
+    devTools: "Development tools",
+    deployTools: "Deployment tools",
+  },
+  {
+    lang: "FR",
+    frameworks: "Frameworks",
+    languages: "Langages",
+    dbs: "Bases de données",
+    devTools: "Outils de développement",
+    deployTools: "Outils de déploiement",
+  },
+];
 export const BUSSHI_BIO = [
-    {
-        lang: 'EN',
-        content: 'Full-stack developper freelance, aspiring pentester, infosec lover, check out some of my personnal projects here...'
-    },
-    {
-        lang: 'FR',
-        content: 'Développeur fullstack freelance, passionné de cybersécurité, explorez ici quelques-uns de mes projets personnels...'
-    }
-]
+  {
+    lang: "EN",
+    content:
+      "Full-stack developper freelance, aspiring pentester, infosec lover, check out some of my personnal projects here...",
+  },
+  {
+    lang: "FR",
+    content:
+      "Développeur fullstack freelance, passionné de cybersécurité, explorez ici quelques-uns de mes projets personnels...",
+  },
+];
 export const LINKS = [
-    {
-        id: 1,
-        name: 'Linkedin',
-        url: 'https://www.linkedin.com/in/alexandre-dubar/',
-    },
-    {
-        id: 2,
-        name: 'Malt',
-        url: 'https://www.malt.fr/profile/alexandredubar',
-     },
-     {
-         id: 3,
-         name: 'Github',
-         url: 'https://github.com/busshi',
-     },
-     {
-         id: 4,
-         name: 'DockerHub',
-         url: 'https://hub.docker.com/repository/docker/busshi/portfolio',
-     }
-]
+  {
+    id: 1,
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/alexandre-dubar/",
+  },
+  {
+    id: 2,
+    name: "Malt",
+    url: "https://www.malt.fr/profile/alexandredubar",
+  },
+  {
+    id: 3,
+    name: "Github",
+    url: "https://github.com/busshi",
+  },
+  {
+    id: 4,
+    name: "DockerHub",
+    url: "https://hub.docker.com/repository/docker/busshi/portfolio",
+  },
+];
