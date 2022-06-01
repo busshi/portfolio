eabefcc230bc9f442225d837fd8985d09fa4b58e
busshi <busshi973@icloud.com>
2022-06-01 16:04:27 +0200

    detect browser language

--- app/context/langage/LangageProvider.tsx
+++ app/context/langage/LangageProvider.tsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import langageContext from "./langageContext";
 
 const LangageProvider: React.FC = ({ children }) => {
-	const [lang, setLang] = useState('EN');
-    const [langId, setLangId] = useState<number>(0);
-  
-	return (
-		<langageContext.Provider
-			value={{
-				lang,
-                setLang,
-                langId,
-                setLangId
-			}}
-		>
-			{children}
-		</langageContext.Provider>
-	);
+  const [lang, setLang] = useState("EN");
+  const [langId, setLangId] = useState<number>(0);
+
+  useEffect(() => {
+    const userLanguage = window.navigator.language;
+    if (userLanguage) {
+      const language = userLanguage.substring(0, 2).toUpperCase();
+      if (language === "FR") {
+        setLang(language);
+        setLangId(1);
+      }
+    }
+  }, []);
+
+  return (
+    <langageContext.Provider
+      value={{
+        lang,
+        setLang,
+        langId,
+        setLangId,
+      }}
+    >
+      {children}
+    </langageContext.Provider>
+  );
 };
 
 export default LangageProvider;
